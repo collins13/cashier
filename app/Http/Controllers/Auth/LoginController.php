f5d903e9555ffb16b1f1f513bc0a46f55d6a566e
collins13 <rashidcollins16@gmil.com>
2019-09-12 14:06:00 +0300

    initial commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\User;
+use Socialite;
+use Auth;
+use Exception;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+
+    public function callback()
+    {
+        try {
+            
+        
+            $googleUser = Socialite::driver('google')->user();
+            $existUser = User::where('email',$googleUser->email)->first();
+            
+
+            if($existUser) {
+                Auth::loginUsingId($existUser->id);
+            }
+            else {
+                $user = new User;
+                $user->name = $googleUser->name;
+                $user->email = $googleUser->email;
+                $user->google_id = $googleUser->id;
+                $user->password = md5(rand(1,10000));
+                $user->save();
+                Auth::loginUsingId($user->id);
+            }
+            return redirect()->to('/home');
+        } 
+        catch (Exception $e) {
+            return 'error';
+        }
+    }
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
---
+++ app/Http/Controllers/SocialAuthGoogleController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Socialite;
+use Auth;
+use Exception;
+
+class SocialAuthGoogleController extends Controller
+{
+
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+
+    public function callback()
+    {
+        try {
+            
+        
+            $googleUser = Socialite::driver('google')->user();
+            $existUser = User::where('email',$googleUser->email)->first();
+            
+
+            if($existUser) {
+                Auth::loginUsingId($existUser->id);
+            }
+            else {
+                $user = new User;
+                $user->name = $googleUser->name;
+                $user->email = $googleUser->email;
+                $user->google_id = $googleUser->id;
+                $user->password = md5(rand(1,10000));
+                $user->save();
+                Auth::loginUsingId($user->id);
+            }
+            return redirect()->to('/home');
+        } 
+        catch (Exception $e) {
+            return 'error';
+        }
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
